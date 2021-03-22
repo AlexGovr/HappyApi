64670dbbc7852730a4eaec148a1fbe37948f61a2
alex <sobachka.94@mail.ru>
2021-03-22 14:16:12 -0400

    upd urls

--- happy/urls.py
+++ happy/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from couriers import restviews
+from rest_framework.routers import DefaultRouter
+
+# Create a router and register viewsets
+router = DefaultRouter()
+router.register('couriers', restviews.CourierViewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
