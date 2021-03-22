4114b1e375adffbba6117d044e27959ea8738a64
alex <sobachka.94@mail.ru>
2021-03-22 13:36:51 -0400

    upd: add model

--- couriers/models.py
+++ couriers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Courier(models.Model):
+    type_choices = [('foot', 'foot'), ('bike', 'bike'), ('car', 'car'),]
+    courier_id = models.IntegerField(primary_key=True)
+    courier_type = models.CharField(max_length=20, choices=type_choices)
+    regions = models.CharField(max_length=120, default='')
+    working_hours = models.CharField(max_length=240, default='')
