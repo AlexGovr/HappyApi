7e97fde13c653f47493081b7509447640db40981
alex <sobachka.94@mail.ru>
2021-03-23 14:45:42 -0400

    add time parsing functions

--- couriers/serializers.py
+++ couriers/serializers.py
@@ -1,5 +1,10 @@
-from .models import Courier
+from time import strptime
 from rest_framework import serializers
+from .models import Courier
+
+
+class WrongTimeFormat(BaseException):
+    pass
 
 
 class CourierSerializer(serializers.ModelSerializer):
@@ -7,3 +12,24 @@ class CourierSerializer(serializers.ModelSerializer):
     class Meta:
         model = Courier
         fields = '__all__'
+
+    def create(self, validated_data):
+        working_hours = validated_data['working_hours']
+        # check time format
+        _ = [parse_time(t) for t in working_hours]
+
+
+def parse_time_interval(timeinterval):
+    stamps = timeinterval.split('-')
+    if len(stamps) != 2:
+        raise WrongTimeFormat()
+    begin = parse_time(stamps[0])
+    end = parse_time(stamps[1])
+    return begin, end
+
+
+def parse_time(time):
+    try:
+        return strptime(time, '%H:%M:%S')
+    except:
+        raise WrongTimeFormat()
