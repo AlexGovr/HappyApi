154ae73270c279204c45b3bbd534af172047a82d
alex <sobachka.94@mail.ru>
2021-03-24 03:10:21 -0400

    post couriers: proper data fields

--- couriers/restviews.py
+++ couriers/restviews.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 from .serializers import CourierSerializer
 from .models import Courier
 
@@ -7,3 +8,11 @@ class CourierViewset(viewsets.ModelViewSet):
 
     serializer_class = CourierSerializer
     queryset = Courier.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        data = request.data['data']
+        serializer = self.get_serializer(data=data, many=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
