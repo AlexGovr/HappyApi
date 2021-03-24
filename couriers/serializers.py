3b4422dff7797785318fc6333b8d47da72b7f487
alex <sobachka.94@mail.ru>
2021-03-24 01:46:29 -0400

    update serializing and reprersentin time intervals

--- couriers/serializers.py
+++ couriers/serializers.py
@@ -1,35 +1,50 @@
-from time import strptime
+from time import strptime, strftime
 from rest_framework import serializers
 from .models import Courier
 
 
-class WrongTimeFormat(BaseException):
-    pass
-
-
 class CourierSerializer(serializers.ModelSerializer):
+
+    working_hours = serializers.CharField(required=True)
 
     class Meta:
         model = Courier
         fields = '__all__'
 
-    def create(self, validated_data):
-        working_hours = validated_data['working_hours']
-        # check time format
-        _ = [parse_time(t) for t in working_hours]
+    def to_internal_value(self, data):
+        whours = data['working_hours']
+        if not isinstance(whours, list):
+            raise serializers.ValidationError('working hours intervals must be contained in a list')
+        data['working_hours'] = ','.join(whours)
+        return super().to_internal_value(data)
+
+    def to_representation(self, inst):
+        data = super().to_representation(inst)
+        whours = data['working_hours']
+        data['working_hours'] = whours.split(',')
+        return data
+
+    def validate_working_hours(self, value):
+        _time = [parse_timeint(t) for t in value.split(',')]
+        _str_list = [srl_timeint(*t) for t in _time]
+        _str = ','.join(_str_list)
+        return _str
 
 
-def parse_time_interval(timeinterval):
+def parse_timeint(timeinterval):
     stamps = timeinterval.split('-')
-    if len(stamps) != 2:
-        raise WrongTimeFormat()
     begin = parse_time(stamps[0])
     end = parse_time(stamps[1])
     return begin, end
 
 
-def parse_time(time):
-    try:
-        return strptime(time, '%H:%M:%S')
-    except:
-        raise WrongTimeFormat()
+def parse_time(time_str):
+    return strptime(time_str, '%H:%M')
+
+
+def srl_timeint(begin, end):
+    return f'{srl_time(begin)}-{srl_time(end)}'
+
+
+def srl_time(time):
+    return strftime('%H:%M', time)
