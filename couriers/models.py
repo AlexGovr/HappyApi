bcaf01bba909b1ca94aeadb8e4592d2b68164ac5
alex <sobachka.94@mail.ru>
2021-03-25 03:00:10 -0400

    Courier: make fields required

--- couriers/models.py
+++ couriers/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Courier(models.Model):
     type_choices = [('foot', 'foot'), ('bike', 'bike'), ('car', 'car'),]
     courier_id = models.IntegerField(primary_key=True)
-    courier_type = models.CharField(max_length=20, choices=type_choices)
-    regions = models.CharField(max_length=120, default='')
-    working_hours = models.CharField(max_length=240, default='')
+    courier_type = models.CharField(max_length=20, choices=type_choices, blank=False)
+    regions = models.CharField(max_length=120, blank=False)
+    working_hours = models.CharField(max_length=240, blank=False)
 
     payload_dict = {
         'foot': 10,
