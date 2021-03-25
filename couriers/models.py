d94153184cbdb655f7dc123f4a75a3fd8994f635
alex <sobachka.94@mail.ru>
2021-03-25 01:50:36 -0400

    Courier: add payload property

--- couriers/models.py
+++ couriers/models.py
@@ -7,3 +7,13 @@ class Courier(models.Model):
     courier_type = models.CharField(max_length=20, choices=type_choices)
     regions = models.CharField(max_length=120, default='')
     working_hours = models.CharField(max_length=240, default='')
+
+    payload_dict = {
+        'foot': 10,
+        'bike': 15,
+        'car': 50,
+    }
+
+    @property
+    def payload(self):
+        return __class__.payload_dict[self.courier_type]
