54bb804ddea4728e097dd32a55a92dc7131e42c6
alex <sobachka.94@mail.ru>
2021-03-27 10:23:49 -0400

    debug order assigning

--- couriers/models.py
+++ couriers/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from django.db import models
-from .time_parse import parse_timeint, parse_time, srl_timeint, srl_time
+from .time_parse import (parse_timeint, parse_time, datetime_now,
+                            srl_timeint, srl_time)
 
 
 class Courier(models.Model):
@@ -17,29 +18,28 @@ class Courier(models.Model):
     }
 
     def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.whours = [parse_timeint(tint) for tint
                         in self.working_hours.split(',')]
-        super().__init__(*args, **kwargs)
 
     @property
     def payload(self):
         return __class__.payload_dict[self.courier_type]
 
     def assign_orders(self, orders):
-        assign_time = datetime.now()
+        assign_time = datetime_now()
         orders = self.filter_orders(orders)
-        chosen = opt_byweight(orders, self.payload)
+        _, chosen = opt_byweight(orders, self.payload)
         if not chosen:
             return None, None
         for ordr in chosen:
-            ordr.courier_id = self.courier_id
+            ordr.courier_id = self
             ordr.assign_time = assign_time
-            # ordr.save()
-        assign_time = srl_time(assign_time)
+            ordr.save()
         return chosen, assign_time
 
     def filter_orders(self, orders):
-        orders = orders.filter(courier_id=None, weight_le=self.payload)
+        orders = orders.filter(courier_id=None, weight__lte=self.payload)
         orders = self.filter_byregions(orders)
         orders = self.filter_by_time(orders)
         return orders
@@ -54,7 +54,7 @@ class Courier(models.Model):
     def filter_by_time(self, orders):
         orders = [ordr for ordr in orders
                     if ordr._fits_schedule(self.whours)]
-        return sorted(orders, reverse=True)
+        return sorted(orders, reverse=True, key=lambda o: o.weight)
 
 
 class Order(models.Model):
@@ -68,9 +68,9 @@ class Order(models.Model):
     complete_time = models.DateTimeField(null=True)
 
     def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.dhours = [parse_timeint(tint) for tint
                         in self.delivery_hours.split(',')]
-        super().__init__(*args, **kwargs)
 
     def _fits_schedule(self, whours):
         for b, e in whours:
