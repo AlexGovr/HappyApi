9dae33982eb57a9a393ce8a047bbcc6707004297
Alex <alex_govr@mail.ru>
2021-03-29 18:33:21 +0300

    request error details

--- couriers/restviews.py
+++ couriers/restviews.py
@@ -30,10 +30,7 @@ class BaseViewset(viewsets.ModelViewSet):
                 allsrl.append(srl)
                 resp_ids.append({'id': _dat[self.id_fieldname]})
             else:
-                f_srl = self.get_serializer
-                errors = [{'id': _dat[self.id_fieldname]}
-                            for _dat in data[i:]
-                            if not f_srl(data=_dat).is_valid()]
+                errors = self.collect_errors(data[i:])
                 resp_data = {'validation_error':{self.resp_data_key: errors}}
                 return Response(resp_data, status=status.HTTP_400_BAD_REQUEST)
 
@@ -41,6 +38,18 @@ class BaseViewset(viewsets.ModelViewSet):
         headers = self.get_success_headers(allsrl[0].data)
         resp_data = {self.resp_data_key: resp_ids}
         return Response(resp_data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def collect_errors(self, data):
+        errdata = []
+        for _dat in data:
+            srl = self.get_serializer(data=_dat)
+            if not srl.is_valid():
+                dtl = {'id': _dat[self.id_fieldname]}
+                errors = srl.errors
+                add = {field: str(err[0]) for (field, err) in errors.items()}
+                dtl.update({'detail': add})
+                errdata.append(dtl)
+        return errdata
 
 
 class CourierViewset(BaseViewset):
