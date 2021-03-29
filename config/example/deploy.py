558b6fd32aaf1a358950fc3c8b64781903fa0f77
Alex <alex_govr@mail.ru>
2021-03-29 19:54:19 +0300

    update config

--- config/example/deploy.py
+++ config/example/deploy.py
@@ -1,3 +1,14 @@
 
 DEBUG = True
 ALLOWED_HOSTS = ['localhost']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'happy',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5433',
+    }
+}
