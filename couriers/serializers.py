26d8629fca6fc965d0c591bd1f54121fbbfca789
Alexander <77458675+AlexGovr@users.noreply.github.com>
2021-04-03 10:54:06 +0300

    add "time interval beginning >= ending" error

--- couriers/serializers.py
+++ couriers/serializers.py
@@ -1,4 +1,6 @@
+from django.core.exceptions import ValidationError as DjangoValidationError
 from rest_framework import serializers
+from rest_framework.fields import empty
 from rest_framework.exceptions import ValidationError
 from .models import Courier, Order
 from .time_parse import parse_timeint, srl_timeint
@@ -21,7 +23,7 @@ class CustomTimeSerializer(serializers.ModelSerializer):
         if whours:
             # raise if not in list
             if not isinstance(whours, list):
-                s = (f'{self.time_field_name} values intervals'
+                s = (f'{self.time_field_name} values intervals '
                      + 'must be contained in a list')
                 raise serializers.ValidationError(s)
             data[self.time_field_name] = ','.join(whours)
@@ -46,9 +48,37 @@ class CustomTimeSerializer(serializers.ModelSerializer):
             _time = [parse_timeint(t) for t in value.split(',')]
         except ValueError as e:
             raise ValidationError(detail=str(e))
+        self.check_time_intervals(_time)
         _str_list = [srl_timeint(*t) for t in _time]
         _str = ','.join(_str_list)
         return _str
+
+    def run_validation(self, data=empty):
+        """
+        override to handle custom error
+        """
+        (is_empty_value, data) = self.validate_empty_values(data)
+        if is_empty_value:
+            return data
+
+        try:
+            # put this row under try-except to handle
+            # "working_hours field must be contained in a list" error
+            value = self.to_internal_value(data)
+            self.run_validators(value)
+            value = self.validate(value)
+            assert (value is not None,
+                    '.validate() should return the validated data')
+        except (ValidationError, DjangoValidationError) as exc:
+            raise ValidationError(detail=serializers.as_serializer_error(exc))
+
+        return value
+
+    def check_time_intervals(self, intvs):
+        for b, e in intvs:
+            if b >= e:
+                raise ValidationError('working_hours beginning '
+                                      'must be greater than ending')
 
 
 class CourierSerializer(CustomTimeSerializer):
